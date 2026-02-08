7c23544a4a1704a7fcae78df15af4780d64d7168
FilipeGuerreiro <filipe.guerreiro@sptech.school>
2026-02-08 17:50:22 -0300

    feat: add userDao implementation placeholder for iOS database

--- composeApp/src/iosMain/kotlin/filipe/guerreiro/data/local/AppDatabaseConstructor.kt
+++ composeApp/src/iosMain/kotlin/filipe/guerreiro/data/local/AppDatabaseConstructor.kt
@@ -4,11 +4,13 @@ import androidx.room.InvalidationTracker
 import androidx.room.RoomDatabaseConstructor
 import filipe.guerreiro.data.local.dao.CashDao
 import filipe.guerreiro.data.local.dao.TransactionDao
+import filipe.guerreiro.data.local.dao.UserDao
 
 object AppDatabaseConstructorPlatform : RoomDatabaseConstructor<AppDatabase> {
     override fun initialize(): AppDatabase = object : AppDatabase() {
         override fun cashDao(): CashDao = throw NotImplementedError("iOS DB not initialized in this build")
         override fun transactionDao(): TransactionDao = throw NotImplementedError("iOS DB not initialized in this build")
+        override fun userDao(): UserDao = throw NotImplementedError("iOS DB not initialized in this build")
 
         override fun createInvalidationTracker(): InvalidationTracker = throw NotImplementedError("iOS DB not initialized")
     }
